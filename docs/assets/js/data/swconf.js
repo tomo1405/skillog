8bfa510a855026ea6e6631e25cd107be3a843a2d
tomo1405 <tomo20020510@icloud.com>
2026-03-19 20:05:47 +0900

    add img and docs

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1773918027',resources: [
+      '/skillog/assets/css/jekyll-theme-chirpy.css',
+      '/skillog/',
+      
+        '/skillog/categories/',
+      
+        '/skillog/tags/',
+      
+        '/skillog/archives/',
+      
+        '/skillog/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
